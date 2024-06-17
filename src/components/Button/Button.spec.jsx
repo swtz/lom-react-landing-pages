4c87aae51fe8608dd131b015cadeca4c14bbed43
Leonardo Lüders <luderser@hotmail.com>
2024-06-17 11:21:36 -0300

    learning about snapshot test

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -35,4 +35,9 @@ describe("<Button />", () => {
 
     expect(button).toBeEnabled();
   });
+  it("should match snapshot", () => {
+    const fn = jest.fn();
+    const { container } = render(<Button text="Load more" isDisabled={false} onClick={fn} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
