7ae7625bfdbd10b4694f0bb39e41ee69875cb47e
Leonardo Lüders <luderser@hotmail.com>
2025-01-27 10:53:54 -0300

    finished class 69 (Error Boundaries - Controlando erros).

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,46 +1,35 @@
-import { useDebugValue, useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-const useMediaQuery = (queryValue, initialValue = false) => {
-  const [match, setMatch] = useState(initialValue);
+const s = {
+  style: {
+    fontSize: '60px',
+  },
+};
 
-  useDebugValue(`Query: ${queryValue}`, (value) => {
-    return value + ' additional info';
-  });
+const ItWillThrowError = () => {
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    let isMounted = true;
-    const matchMedia = window.matchMedia(queryValue);
+    console.log(counter);
+    if (counter > 3) {
+      throw new Error('Que chato!!!');
+    }
+  }, [counter]);
 
-    const handleChange = () => {
-      if (!isMounted) return;
-      setMatch(!!matchMedia.matches);
-    };
-
-    matchMedia.addEventListener('change', handleChange);
-    setMatch(!!matchMedia.matches);
-
-    return () => {
-      isMounted = false;
-      matchMedia.removeEventListener('change', handleChange);
-    };
-  }, [queryValue]);
-
-  return match;
+  return (
+    <div>
+      <button {...s} onClick={() => setCounter((c) => c + 1)}>
+        Click to increase {counter}
+      </button>
+    </div>
+  );
 };
 
 export const Home = () => {
-  const huge = useMediaQuery('(min-width: 980px)');
-  const big = useMediaQuery('(max-width: 979px) and (min-width: 768px)');
-  const medium = useMediaQuery('(max-width: 767px) and (min-width: 321px)');
-  const small = useMediaQuery('(max-width: 321px)');
-  const background = huge
-    ? 'green'
-    : big
-    ? 'red'
-    : medium
-    ? 'yellow'
-    : small
-    ? 'purple'
-    : null;
-  return <div style={{ fontSize: '60px', background }}>Olá, mundo!</div>;
+  return (
+    <div {...s}>
+      <ItWillThrowError />
+    </div>
+  );
 };
