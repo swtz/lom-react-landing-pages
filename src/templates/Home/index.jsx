1479acfb9290b8cf357b7d34fe3f7d9d9b07a98a
Leonardo Lüders <luderser@hotmail.com>
2025-01-28 08:25:57 -0300

    finished class 72 React.lazy - Suspense e divisão de código (Code-Splitting)

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,60 +1,22 @@
-/* eslint-disable react/prop-types */
-import { createContext, useContext, useState } from 'react';
+import { lazy, Suspense, useState } from 'react';
 
-const s = {
-  style: {
-    fontSize: '60px',
-  },
-};
+const loadComponent = () => import('./LazyComponent'); // dynamic import
 
-const TurnOnOffContext = createContext();
-
-const TurnOnOff = ({ children }) => {
-  const [isOn, setIsOn] = useState(false);
-  const onTurn = () => setIsOn((s) => !s);
-
-  return (
-    <TurnOnOffContext.Provider value={{ isOn, onTurn }}>
-      {children}
-    </TurnOnOffContext.Provider>
-  );
-};
-
-const TurnedOn = ({ children }) => {
-  const { isOn } = useContext(TurnOnOffContext);
-  return isOn ? children : null;
-};
-
-const TurnedOff = ({ children }) => {
-  const { isOn } = useContext(TurnOnOffContext);
-  return isOn ? null : children;
-};
-
-const TurnButton = ({ ...props }) => {
-  const { isOn, onTurn } = useContext(TurnOnOffContext);
-
-  return (
-    <button onClick={onTurn} {...props}>
-      Turn {isOn ? 'OFF' : 'ON'}
-    </button>
-  );
-};
-
-const P = ({ children }) => <p {...s}>{children}</p>;
+const LazyComponent = lazy(loadComponent);
 
 export const Home = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <TurnOnOff>
-      <div>
-        <p>Hello, world!</p>
-        <TurnedOn>
-          <P>Aqui as coisas que vão acontecer quando estiver ON.</P>
-        </TurnedOn>
-        <TurnedOff>
-          <P>Aqui vem as coisas do OFF.</P>
-        </TurnedOff>
-        <TurnButton {...s} />
-      </div>
-    </TurnOnOff>
+    <div>
+      <p>
+        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
+          Show {show ? 'LC is on screen' : 'LC is off screen'}
+        </button>
+      </p>
+      <Suspense fallback={<p>Loading...</p>}>
+        {show && <LazyComponent />}
+      </Suspense>
+    </div>
   );
 };
