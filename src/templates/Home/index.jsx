7d7e0021b6831e6a5fdb26e95c420880aa735579
Leonardo Lüders <luderser@hotmail.com>
2025-01-28 08:56:30 -0300

    finished class 73 Context API + Hooks (unindo as ideias) - Parte 1.

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,22 +1,7 @@
-import { lazy, Suspense, useState } from 'react';
-
-const loadComponent = () => import('./LazyComponent'); // dynamic import
-
-const LazyComponent = lazy(loadComponent);
-
 export const Home = () => {
-  const [show, setShow] = useState(false);
-
   return (
     <div>
-      <p>
-        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
-          Show {show ? 'LC is on screen' : 'LC is off screen'}
-        </button>
-      </p>
-      <Suspense fallback={<p>Loading...</p>}>
-        {show && <LazyComponent />}
-      </Suspense>
+      <p>Hello, world!</p>
     </div>
   );
 };
