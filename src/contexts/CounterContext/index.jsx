b554d8a4ade9d824421416de37a7112b64935538
Leonardo Lüders <luderser@hotmail.com>
2025-01-28 09:33:12 -0300

    created 'Context', 'CounterContextProvider' components | created 'useCounterContext' hook | created 'initialState' state.

---
+++ src/contexts/CounterContext/index.jsx
@@ -0,0 +1,25 @@
+import P from 'prop-types';
+import { createContext, useContext } from 'react';
+
+export const initialState = {
+  counter: 0,
+  loading: false,
+};
+
+const Context = createContext();
+
+export const CounterContextProvider = ({ children }) => {
+  return (
+    <Context.Provider value={{ initialState }}>{children}</Context.Provider>
+  );
+};
+
+CounterContextProvider.propTypes = {
+  children: P.node.isRequired,
+};
+
+export const useCounterContext = () => {
+  const context = useContext(Context);
+
+  return [...context];
+};
