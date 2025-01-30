6ece1d42852c13bca593bd8d8e7dcd103ac2aa17
Leonardo Lüders <luderser@hotmail.com>
2025-01-30 11:00:15 -0300

    project restructured to do the 8th from course | --removing-files

--- src/components/Heading/index.jsx
+++
@@ -1,7 +0,0 @@
-import { useCounterContext } from '../../contexts/CounterContext';
-
-export const Heading = () => {
-  const [state, actions] = useCounterContext();
-
-  return <h1>{state.counter}</h1>;
-};
