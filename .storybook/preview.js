6c94f0e11f8fd4042f3dc85a2d5648db97558079
Leonardo Lüders <luderser@hotmail.com>
2025-01-31 18:40:00 -0300

    finished class 122 (Configurando o Storybook - Parte 2).

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,19 @@ import { GlobalStyles } from '../src/styles/global-styles';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.primaryColor,
+      },
+    ],
+  },
 };
 
 export const decorators = [
--- src/components/Heading/stories.jsx
+++ src/components/Heading/stories.jsx
@@ -1,15 +1,32 @@
-import { ArgTypes } from '@storybook/blocks';
 import { Heading } from '.';
 
 export default {
   title: 'Heading',
   component: Heading,
   args: {
-    children: 'Alguma coisa para renderizar',
+    children: 'O texto está escuro',
+    light: false,
   },
   argTypes: {
     children: { type: 'string' },
+    light: { type: 'boolean' },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
   },
 };
 
-export const Template = (args) => <Heading {...args} />;
+export const Light = (args) => <Heading {...args} />;
+export const Dark = (args) => <Heading {...args} light={true} />;
+
+Light.parameters = {
+  backgrounds: {
+    default: 'light',
+  },
+};
+
+Dark.args = {
+  children: 'O texto está claro',
+};
