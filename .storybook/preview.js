d7d5cb20c75bfda168cdac629bce952e1d37d516
Leonardo Lüders <luderser@hotmail.com>
2025-01-31 18:08:17 -0300

    finished class 121 (Configurando o Storybook - Parte 1).

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../src/styles/theme';
+import { GlobalStyles } from '../src/styles/global-styles';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+      <GlobalStyles />
+    </ThemeProvider>
+  ),
+];
--- src/components/Heading/stories.jsx
+++ src/components/Heading/stories.jsx
@@ -1,8 +1,15 @@
+import { ArgTypes } from '@storybook/blocks';
 import { Heading } from '.';
 
 export default {
   title: 'Heading',
   component: Heading,
+  args: {
+    children: 'Alguma coisa para renderizar',
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
 };
 
 export const Template = (args) => <Heading {...args} />;
