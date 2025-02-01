7585a14bdcc5991f2fcf9598b9500c97ba976766
Leonardo Lüders <luderser@hotmail.com>
2025-02-01 19:37:47 -0300

    created test to check 'Heading' component behavior when 'colorDark' prop has value as 'true' | created test to check 'font-size' css rule  when 'size' prop receives the value 'small'.

--- src/components/Heading/Heading.test.jsx
+++ src/components/Heading/Heading.test.jsx
@@ -5,13 +5,31 @@ import { theme } from '../../styles/theme';
 
 describe('<Heading />', () => {
   it('should render with default values', () => {
-    const { debug } = renderTheme(<Heading>texto</Heading>);
+    renderTheme(<Heading>texto</Heading>);
     const heading = screen.getByRole('heading', { name: 'texto' });
 
     expect(heading).toHaveStyle({
       color: theme.colors.primaryColor,
       'font-size': theme.font.sizes.xhuge,
       'text-transform': 'none',
+    });
+  });
+
+  it('should render with white color', () => {
+    renderTheme(<Heading colorDark={false}>texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(heading).toHaveStyle({
+      color: theme.colors.white,
+    });
+  });
+
+  it('should render correct heading sizes', () => {
+    renderTheme(<Heading size="small">texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(heading).toHaveStyle({
+      'font-size': theme.font.sizes.medium,
     });
   });
 });
