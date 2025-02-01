7f9b1b0e0b8099d7836c0e69a4bf4c67c2a75d3f
Leonardo Lüders <luderser@hotmail.com>
2025-02-01 19:21:24 -0300

    tests with 'Heading' component with default values done.

---
+++ src/components/Heading/Heading.test.jsx
@@ -0,0 +1,17 @@
+import { screen } from '@testing-library/react';
+import { Heading } from '.';
+import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
+
+describe('<Heading />', () => {
+  it('should render with default values', () => {
+    const { debug } = renderTheme(<Heading>texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(heading).toHaveStyle({
+      color: theme.colors.primaryColor,
+      'font-size': theme.font.sizes.xhuge,
+      'text-transform': 'none',
+    });
+  });
+});
