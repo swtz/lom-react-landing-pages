3364c6b6fb1842f0a9d92094ae73e4adc7ab269e
Leonardo Lüders <luderser@hotmail.com>
2025-02-01 18:03:08 -0300

    added property 'sizes' in the 'theme.js' object to use in the 'Title' styled-component to dynamically call a method from 'titleSize'. The prop 'size' was added too in the 'Heading' component to call those methods from 'titleSize' object. Each method return a 'css rule' using 'css' method from 'styled-components'.

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,9 +1,14 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
-export const Heading = ({ children, colorDark = true, as = 'h1' }) => {
+export const Heading = ({
+  children,
+  colorDark = true,
+  as = 'h1',
+  size = 'big',
+}) => {
   return (
-    <Styled.Title colorDark={colorDark} as={as}>
+    <Styled.Title colorDark={colorDark} as={as} size={size}>
       {children}
     </Styled.Title>
   );
@@ -13,4 +18,5 @@ Heading.propTypes = {
   children: P.node.isRequired,
   colorDark: P.bool.isRequired,
   as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  size: P.oneOf(['big', 'medium', 'small']),
 };
--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -1,7 +1,16 @@
 import styled, { css } from 'styled-components';
 
+const titleSize = {
+  small: (theme) => css`
+    font-size: ${theme.font.sizes.small};
+  `,
+  medium: (theme) => css``,
+  big: (theme) => css``,
+};
+
 export const Title = styled.h1`
-  ${({ theme, colorDark }) => css`
+  ${({ theme, colorDark, size }) => css`
   color: ${colorDark ? theme.colors.primaryColor : theme.colors.white};
+  ${titleSize[size](theme)};
   `}
 `;
