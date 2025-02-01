acf52ce5d55a9016960b7edbb7dcb36dd2cad655
Leonardo Lüders <luderser@hotmail.com>
2025-02-01 18:45:30 -0300

    finished class 126 (Criando o componente de Heading - Parte 2) | some fixes to remove 'warnings' from console.

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
-
   * {
     margin: 0;
     padding: 0;
