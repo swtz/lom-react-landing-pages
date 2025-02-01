d7a51d40550bcd037d076240027641ace78928da
Leonardo Lüders <luderser@hotmail.com>
2025-02-01 19:02:34 -0300

    modified 'App.test.jsx' to just render 'Home' component.

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,12 +1,8 @@
-import { screen } from '@testing-library/react';
 import { Home } from '.';
 import { renderTheme } from '../../styles/render-theme';
 
-test('renders <Home /> component', () => {
-  const { debug } = renderTheme(<Home />);
-  const headingContainer = screen.getByRole('heading', {
-    name: 'Hello',
-  }).parentElement;
-  expect(headingContainer).toMatchSnapshot();
-  expect(headingContainer).toHaveStyleRule('background', 'blue');
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
+  });
 });
