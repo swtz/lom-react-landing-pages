5e6ef8b3e8a24443bf852085d4b4b14ddcc0cf3c
Leonardo Lüders <luderser@hotmail.com>
2025-02-03 15:53:51 -0300

    finished classes 136 (Componente: LogoLink com testes ) | tests with 'LogoLink' component done.

--- src/components/LogoLink/LogoLink.test.jsx
+++ src/components/LogoLink/LogoLink.test.jsx
@@ -1,12 +1,33 @@
+import { screen } from '@testing-library/react';
 import { LogoLink } from '.';
 import { renderTheme } from '../../styles/render-theme';
-import { theme } from '../../styles/theme';
 
 describe('<LogoLink />', () => {
-  it('should render', () => {
-    const { container } = renderTheme(<LogoLink>Children</LogoLink>);
-    expect(container.firstChild).toHaveStyle({
-      color: theme.colors.primaryColor,
-    });
+  it('should render text logo', () => {
+    renderTheme(<LogoLink link="#target" text="Hello, world!" />);
+    expect(
+      screen.getByRole('heading', { name: 'Hello, world!' }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Hello, world!' })).toHaveAttribute(
+      'href',
+      '#target',
+    );
+  });
+
+  it('should render image logo', () => {
+    renderTheme(
+      <LogoLink link="#target" text="Hello, world!" srcImg="image.jpg" />,
+    );
+    expect(screen.getByAltText('Hello, world!')).toHaveAttribute(
+      'src',
+      'image.jpg',
+    );
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(
+      <LogoLink link="#target" text="Hello, world!" srcImg="image.jpg" />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
