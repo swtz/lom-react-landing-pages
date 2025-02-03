d99caf493a520a9d7395c2a0c9005d6469e89e07
Leonardo Lüders <luderser@hotmail.com>
2025-02-03 09:10:42 -0300

    finished class 132 (Componente MenuLink com testes) | created 3 tests to 'MenuLink' component. All passed and created a snapshot from it.

--- src/components/MenuLink/MenuLink.test.jsx
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -1,10 +1,65 @@
+import { screen } from '@testing-library/react';
 import { MenuLink } from '.';
 import { renderTheme } from '../../styles/render-theme';
 
 describe('<MenuLink />', () => {
-  it('should render correct anchor element', () => {
-    const { container } = renderTheme(<MenuLink>link</MenuLink>);
-    const a = container.querySelector('a');
-    expect(a.tagName.toLowerCase()).toBe('a');
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="https://localhost">link</MenuLink>);
+    expect(screen.getByRole('link', { name: 'link' })).toHaveAttribute(
+      'target',
+      '_self',
+    );
+  });
+
+  it('should open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="https://localhost" newTab>
+        link
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'link' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
+
+  it('should match snapshot', () => {
+    renderTheme(<MenuLink link="https://localhost">link</MenuLink>);
+    expect(screen.getByRole('link', { name: 'link' })).toMatchInlineSnapshot(`
+      .c0 {
+        display: block;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+        font-size: 1.6rem;
+        padding: 1.6rem;
+        color: #0A1128;
+        position: relative;
+      }
+
+      .c0::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        width: 0;
+        height: 0.2rem;
+        background: #DC143C;
+        -webkit-transition: all 300ms ease-in-out;
+        transition: all 300ms ease-in-out;
+      }
+
+      .c0:hover::after {
+        left: 25%;
+        width: 50%;
+      }
+
+      <a
+        class="c0"
+        href="https://localhost"
+        target="_self"
+      >
+        link
+      </a>
+    `);
   });
 });
