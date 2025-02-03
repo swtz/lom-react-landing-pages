183220d14fddf7181ff291a0f145545a9413abb1
Leonardo Lüders <luderser@hotmail.com>
2025-02-03 10:38:57 -0300

    stylized 'NavLinks' component.

--- src/components/NavLinks/styles.js
+++ src/components/NavLinks/styles.js
@@ -2,8 +2,13 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   ${({ theme }) => css`
-  background: #fff;
+    background: #fff;
     display: flex;
     flex-flow: row wrap;
+
+    @media ${theme.media.lteMedium} {
+      flex-flow: column wrap;
+      align-content: center;
+    }
   `};
 `;
