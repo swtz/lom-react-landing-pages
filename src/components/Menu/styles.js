14089ce9aea13ddcedd24bfb72162e8db82ece4c
Leonardo Lüders <luderser@hotmail.com>
2025-02-04 10:30:22 -0300

    beginned 'Menu' component stylization | added 'mediumGray' property in the 'theme.js' object.

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,5 +1,15 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+    position: fixed;
+    z-index: 5;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    border-bottom: ${theme.colors.mediumGray};
+  `}
 `;
