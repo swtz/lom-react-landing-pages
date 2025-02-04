7077e0fed447e9246841897f7cc2c2fd4a7969ce
Leonardo Lüders <luderser@hotmail.com>
2025-02-04 09:37:02 -0300

    'SectionBackground' component done.

--- src/components/SectionBackground/index.jsx
+++ src/components/SectionBackground/index.jsx
@@ -1,14 +1,13 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
-export const SectionBackground = ({ children }) => {
+export const SectionBackground = ({ children, background = false }) => {
   return (
-    <Styled.Container>
-      <h1>{children}</h1>
-    </Styled.Container>
+    <Styled.Container background={background}>{children}</Styled.Container>
   );
 };
 
 SectionBackground.propTypes = {
   children: P.node.isRequired,
+  background: P.bool,
 };
--- src/components/SectionBackground/stories.jsx
+++ src/components/SectionBackground/stories.jsx
@@ -1,13 +1,26 @@
 import { SectionBackground } from '.';
+import { SectionContainer } from '../SectionContainer';
 
 export default {
   title: 'SectionBackground',
   component: SectionBackground,
   args: {
-    children: 'SectionBackground',
+    children: (
+      <SectionContainer>
+        <div>
+          <h1>SectionContainer</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+            itaque consequuntur laborum dolor facere rem voluptas commodi ullam,
+            non, eius eos doloribus debitis nostrum? Obcaecati accusantium a
+            suscipit ab ea.
+          </p>
+        </div>
+      </SectionContainer>
+    ),
   },
   argsTypes: {
-    children: { type: 'string' },
+    children: { disable: true },
   },
 };
 
