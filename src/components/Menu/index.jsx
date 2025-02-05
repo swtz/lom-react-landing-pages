7c4b7860bd879fdfa632199a9abb8a9a4bafc26f
Leonardo Lüders <luderser@hotmail.com>
2025-02-05 08:15:01 -0300

    installed '@styled-icons/material-outlined' package to use icons as components | imported 'Menu as MenuIcon' from this lib and it was involved into 'Styled.Button' styled-component in the 'Menu' component outside 'Styled.Container' styled-component.'

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,17 +3,23 @@ import * as Styled from './styles';
 import { SectionContainer } from '../SectionContainer';
 import { LogoLink } from '../LogoLink';
 import { NavLinks } from '../NavLinks';
+import { Menu as MenuIcon } from '@styled-icons/material-outlined/Menu';
 
 export const Menu = ({ links = [], logoData }) => {
   return (
-    <Styled.Container>
-      <SectionContainer>
-        <Styled.MenuContainer>
-          <LogoLink {...logoData} />
-          <NavLinks links={links} />
-        </Styled.MenuContainer>
-      </SectionContainer>
-    </Styled.Container>
+    <>
+      <Styled.Button>
+        <MenuIcon />
+      </Styled.Button>
+      <Styled.Container>
+        <SectionContainer>
+          <Styled.MenuContainer>
+            <LogoLink {...logoData} />
+            <NavLinks links={links} />
+          </Styled.MenuContainer>
+        </SectionContainer>
+      </Styled.Container>
+    </>
   );
 };
 
