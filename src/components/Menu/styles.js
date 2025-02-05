a68b63c75642b46382956cd667fcb6f691168a0e
Leonardo Lüders <luderser@hotmail.com>
2025-02-05 10:00:15 -0300

    removed 'bottom' and 'height' css properties to display the 'border-bottom' css property in the 'Container' styled-component.

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -14,10 +14,8 @@ export const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
-    bottom: 0;
     width: 100%;
-    height: 100%;
-    border-bottom: ${theme.colors.mediumGray};
+    border-bottom: 0.1rem solid ${theme.colors.mediumGray};
     background: ${theme.colors.white};
     transition: all 300ms ease-in-out;
 
