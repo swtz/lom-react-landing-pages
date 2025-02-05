4c4e082bea7fc96b23b64dd2a6999e93ef70d161
Leonardo Lüders <luderser@hotmail.com>
2025-02-05 08:53:34 -0300

    created a 'visible' state to use in a ternary operator to display the correct 'icon-component' based in its boolean value | the 'visible' state was passed to 'Styled.Container' styled-component as prop.

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -4,14 +4,16 @@ import { SectionContainer } from '../SectionContainer';
 import { LogoLink } from '../LogoLink';
 import { NavLinks } from '../NavLinks';
 import { Menu as MenuIcon } from '@styled-icons/material-outlined/Menu';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
+import { useState } from 'react';
 
 export const Menu = ({ links = [], logoData }) => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <>
-      <Styled.Button>
-        <MenuIcon />
-      </Styled.Button>
-      <Styled.Container>
+      <Styled.Button>{visible ? <CloseIcon /> : <MenuIcon />}</Styled.Button>
+      <Styled.Container visible={visible}>
         <SectionContainer>
           <Styled.MenuContainer>
             <LogoLink {...logoData} />
