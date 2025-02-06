4d01ee6ef42d74b42cac9d714020eaf91e136bbd
Leonardo Lüders <luderser@hotmail.com>
2025-02-06 10:36:05 -0300

    stylized 'Grid' styled-component.

--- src/components/GridSection/styles.js
+++ src/components/GridSection/styles.js
@@ -1,13 +1,21 @@
 import styled, { css } from 'styled-components';
 import { Title as HeadingContainer } from '../Heading/styles';
+import { Container as TextComponent } from '../TextComponent/styles';
 
 export const Container = styled.div`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+    ${TextComponent} {
+      margin-bottom: ${theme.spacings.xhuge};
+    }
+  `}
 `;
 
 export const Grid = styled.div`
   ${({ theme }) => css`
-  counter-reset: grid-counter;
+    counter-reset: grid-counter;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+    gap: ${theme.spacings.large};
   `}
 `;
 
