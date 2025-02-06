0ccb5700de665e522315f95d61705ca7ab0ba6a7
Leonardo Lüders <luderser@hotmail.com>
2025-02-06 10:22:53 -0300

    created pseudo-element for 'GridElement' styled-component.

--- src/components/GridSection/styles.js
+++ src/components/GridSection/styles.js
@@ -1,13 +1,31 @@
 import styled, { css } from 'styled-components';
+import { Title as HeadingContainer } from '../Heading/styles';
 
 export const Container = styled.div`
   ${({ theme }) => css``}
 `;
 
 export const Grid = styled.div`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+  counter-reset: grid-counter;
+  `}
 `;
 
 export const GridElement = styled.div`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+  ${HeadingContainer} {
+    position: relative;
+    left: 5rem;
+  }
+
+  ${HeadingContainer}::before {
+    counter-increment: grid-counter;
+    content: counter(grid-counter);
+    position: absolute;
+    font-size: 7rem;
+    top: -3rem;
+    left: -5rem;
+    transform: rotate(15deg);
+  }
+  `}
 `;
