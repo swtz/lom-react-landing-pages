a2fbf5f69a41839bc67d1677e7ea9e56117d36e7
Leonardo Lüders <luderser@hotmail.com>
2025-02-07 09:48:55 -0300

    stylized 'GoTop' component | added 'scroll-behavior' css property in the 'styles/global-styles.js' file.

--- src/components/GoTop/stories.jsx
+++ src/components/GoTop/stories.jsx
@@ -14,6 +14,42 @@ export default {
 export const Template = (args) => {
   return (
     <div style={{ height: '400vh' }}>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        labore dicta at minima accusamus, velit ut rem sunt atque modi amet
+        saepe ratione accusantium corporis eum, assumenda et laudantium
+        dignissimos.
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        labore dicta at minima accusamus, velit ut rem sunt atque modi amet
+        saepe ratione accusantium corporis eum, assumenda et laudantium
+        dignissimos.
+      </p>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        labore dicta at minima accusamus, velit ut rem sunt atque modi amet
+        saepe ratione accusantium corporis eum, assumenda et laudantium
+        dignissimos.
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        labore dicta at minima accusamus, velit ut rem sunt atque modi amet
+        saepe ratione accusantium corporis eum, assumenda et laudantium
+        dignissimos.
+      </p>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        labore dicta at minima accusamus, velit ut rem sunt atque modi amet
+        saepe ratione accusantium corporis eum, assumenda et laudantium
+        dignissimos.
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        labore dicta at minima accusamus, velit ut rem sunt atque modi amet
+        saepe ratione accusantium corporis eum, assumenda et laudantium
+        dignissimos.
+      </p>
       <GoTop {...args} />
     </div>
   );
--- src/components/GoTop/styles.js
+++ src/components/GoTop/styles.js
@@ -1,5 +1,17 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.a`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+    position: fixed;
+    background: ${theme.colors.primaryColor};
+    color: ${theme.colors.white};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 2rem;
+    right: 2rem;
+    width: 4rem;
+    height: 4rem;
+    z-index: 6;
+  `}
 `;
