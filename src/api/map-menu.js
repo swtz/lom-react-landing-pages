ed11d06b9f11c921a4d308c6115b95bc229f28d1
Leonardo Lüders <luderser@hotmail.com>
2025-02-10 09:38:03 -0300

    created 'mapMenuLinks' function to process 'links' array following the structure of 'lom-dados.json'. Test with filled 'links' array done.

--- src/api/map-menu.js
+++ src/api/map-menu.js
@@ -16,6 +16,18 @@ export const mapMenu = (menu = {}) => {
   };
 };
 
-export const mapMenuLinks = (links) => {
-  return links;
+export const mapMenuLinks = (links = []) => {
+  return links.map((item) => {
+    const {
+      open_in_new_tab: newTab = false,
+      link_text: children = '',
+      url: link = '',
+    } = item;
+
+    return {
+      newTab,
+      children,
+      link,
+    };
+  });
 };
--- src/api/map-menu.test.js
+++ src/api/map-menu.test.js
@@ -8,4 +8,34 @@ describe('map-menu', () => {
     expect(menu.srcImg).toBe('');
     expect(menu.link).toBe('');
   });
+
+  it('should map menu to match keys and values required', () => {
+    const menu = mapMenu({
+      open_in_new_tab: false,
+      logo_text: 'Landing Page',
+      logo_link: '#home',
+      menu: [
+        {
+          open_in_new_tab: false,
+          link_text: 'intro',
+          url: '#intro',
+        },
+        {
+          open_in_new_tab: false,
+          link_text: 'grid-one',
+          url: '#grid-one',
+        },
+      ],
+      logo: {
+        url: 'a.svg',
+      },
+    });
+    expect(menu.newTab).toBe(false);
+    expect(menu.text).toBe('Landing Page');
+    expect(menu.srcImg).toBe('a.svg');
+    expect(menu.link).toBe('#home');
+    expect(menu.links[0].newTab).toBe(false);
+    expect(menu.links[0].children).toBe('intro');
+    expect(menu.links[0].link).toBe('#intro');
+  });
 });
