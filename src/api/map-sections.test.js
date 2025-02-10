318278a62f1fb6c6dfb9593138210eee373e5089
Leonardo Lüders <luderser@hotmail.com>
2025-02-10 11:08:48 -0300

    finished class 157 (Mapeando SectionTwoColumn com testes).

--- src/api/map-sections.test.js
+++ src/api/map-sections.test.js
@@ -1,7 +1,43 @@
-import { mapSectionTwoColumns } from './map-sections';
+import { mapSections, mapSectionTwoColumns } from './map-sections';
 
 describe('map-sections', () => {
-  it('should map section two columns', () => {
+  it('should render predefined section if no data', () => {
+    const data = mapSections();
+    expect(data).toEqual([]);
+  });
+
+  it('should return a predefined object if no data', () => {
     const data = mapSectionTwoColumns();
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('');
+    expect(data.sectionId).toBe('');
+    expect(data.srcImg).toBe('');
+    expect(data.text).toBe('');
+    expect(data.title).toBe('');
+  });
+
+  it('should map section two columns if there is data', () => {
+    const data = mapSectionTwoColumns({
+      __component: 'section.section-two-columns',
+      title: 'this is the title of the section',
+      description: 'Lorem ipsum dolor sit amet.',
+      image: {
+        data: {
+          attributes: {
+            url: 'a.svg',
+          },
+        },
+      },
+      metadata: {
+        section_id: 'home',
+        background: true,
+      },
+    });
+    expect(data.background).toBe(true);
+    expect(data.component).toBe('section.section-two-columns');
+    expect(data.sectionId).toBe('home');
+    expect(data.srcImg).toBe('a.svg');
+    expect(data.text).toBe('Lorem ipsum dolor sit amet.');
+    expect(data.title).toBe('this is the title of the section');
   });
 });
