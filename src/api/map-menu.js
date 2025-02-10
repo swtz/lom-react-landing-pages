ce6e91a038120334d05e45d753aee9a7b3d2fc4b
Leonardo Lüders <luderser@hotmail.com>
2025-02-10 10:10:19 -0300

    finished class 156 (Testes com TDD: mapeando os menus).

--- src/api/map-menu.js
+++ src/api/map-menu.js
@@ -1,14 +1,16 @@
 export const mapMenu = (menu = {}) => {
   const {
-    open_in_new_tab: newTab = false,
     logo_text: text = '',
     logo_link: link = '',
-    logo: { url: srcImg = '' } = '',
-    menu: links = [],
+    logo: {
+      data: {
+        attributes: { url: srcImg = '' },
+      },
+    } = { data: { attributes: {} } },
+    menu_links: links = [],
   } = menu;
 
   return {
-    newTab,
     text,
     link,
     srcImg,
--- src/api/map-menu.test.js
+++ src/api/map-menu.test.js
@@ -3,7 +3,6 @@ import { mapMenu, mapMenuLinks } from './map-menu';
 describe('map-menu', () => {
   it('should return a predefined object if no data', () => {
     const menu = mapMenu();
-    expect(menu.newTab).toBe(false);
     expect(menu.text).toBe('');
     expect(menu.srcImg).toBe('');
     expect(menu.link).toBe('');
@@ -11,10 +10,9 @@ describe('map-menu', () => {
 
   it('should map menu to match keys and values required', () => {
     const menu = mapMenu({
-      open_in_new_tab: false,
       logo_text: 'Landing Page',
       logo_link: '#home',
-      menu: [
+      menu_links: [
         {
           open_in_new_tab: false,
           link_text: 'intro',
@@ -27,10 +25,13 @@ describe('map-menu', () => {
         },
       ],
       logo: {
-        url: 'a.svg',
+        data: {
+          attributes: {
+            url: 'a.svg',
+          },
+        },
       },
     });
-    expect(menu.newTab).toBe(false);
     expect(menu.text).toBe('Landing Page');
     expect(menu.srcImg).toBe('a.svg');
     expect(menu.link).toBe('#home');
