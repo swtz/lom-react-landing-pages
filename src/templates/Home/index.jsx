293dc3deae5cc7db1f202cd3edcc47c9b3502b06
Leonardo Lüders <luderser@hotmail.com>
2025-02-11 18:24:38 -0300

    created 'data' state to do some checks while the data from api  are loading | added a example in the 'annotations.txt' about how to get the data from Strapi API v4 (after break-changes).

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,19 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
 import { mapData } from '../../api/map-data';
 
 export const Home = () => {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     const load = async () => {
       const data = await fetch('http://localhost:1337/api/pages?populate=deep');
       const json = await data.json();
       const pageData = mapData(json);
-      console.log(pageData);
+      setData(pageData[0]);
     };
 
     load();
   }, []);
+
+  if (data === undefined) {
+    return <h1>Página não encontrada.</h1>;
+  }
+
+  if (data && !data.slug) {
+    return <h1>Carregando...</h1>;
+  }
 
   return <Base {...mockBase} />;
 };
