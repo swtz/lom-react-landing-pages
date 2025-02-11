7cbdaa86af473f0a135dc22df24861234b291092
Leonardo Lüders <luderser@hotmail.com>
2025-02-11 18:59:31 -0300

    created a 'Promise' to call a 'setTimeout' function to apply a great delay in the 'setData' method from 'data' state.

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -3,6 +3,7 @@ import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
 import { mapData } from '../../api/map-data';
 import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
 
 export const Home = () => {
   const [data, setData] = useState([]);
@@ -12,11 +13,17 @@ export const Home = () => {
     const load = async () => {
       try {
         const data = await fetch(
-          'http://localhost:404/api/pages?populate=deep',
+          'http://localhost:1337/api/pages?populate=deep',
         );
         const json = await data.json();
         const pageData = mapData(json);
-        setData(pageData[0]);
+
+        await new Promise((r) => {
+          return setTimeout(() => {
+            setData(pageData[0]);
+            r();
+          }, 100000);
+        });
       } catch (e) {
         setData(undefined);
       }
@@ -36,7 +43,7 @@ export const Home = () => {
   }
 
   if (data && !data.slug) {
-    return <h1>Carregando...</h1>;
+    return <Loading />;
   }
 
   return <Base {...mockBase} />;
