885afa1ccb9713ae660fec1eab978a69c9ac4937
Leonardo Lüders <luderser@hotmail.com>
2025-02-11 18:52:39 -0300

    'Loading' component base code done.

---
+++ src/templates/Loading/styles.js
@@ -0,0 +1,13 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 10;
+    background-color: ${theme.colors.primaryColor};
+  `}
+`;
