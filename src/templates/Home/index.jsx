f0d397843297de577ffca772645e820f108a6cfc
Leonardo Lüders <luderser@hotmail.com>
2025-02-12 17:43:01 -0300

    finished class 173 (Adicionando títulos dinâmicos nas páginas).

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -8,6 +8,7 @@ import { GridTwoColumns } from '../../components/GridTwoColumns';
 import { GridContent } from '../../components/GridContent';
 import { GridText } from '../../components/GridText';
 import { GridImage } from '../../components/GridImage';
+import config from '../../config/index';
 
 export const Home = () => {
   const [data, setData] = useState([]);
@@ -16,13 +17,11 @@ export const Home = () => {
 
   useEffect(() => {
     const pathname = location.pathname.replace(/[^a-z0-9-_]/gi, '');
-    const slug = pathname ? pathname : 'look-my-page';
+    const slug = pathname ? pathname : config.defaultSlug;
 
     const load = async () => {
       try {
-        const data = await fetch(
-          `http://localhost:1337/api/pages/?filters[slug]=${slug}&populate=deep`,
-        );
+        const data = await fetch(`${config.url}${slug}&populate=deep`);
         const json = await data.json();
         const pageData = mapData(json);
         setData(pageData[0]);
@@ -40,6 +39,20 @@ export const Home = () => {
       isMounted.current = false;
     };
   }, [location]);
+
+  useEffect(() => {
+    if (data === undefined) {
+      document.title = `Page not found | ${config.siteName}`;
+    }
+
+    if (data && !data.slug) {
+      document.title = `Loading... | ${config.siteName}`;
+    }
+
+    if (data && data.title) {
+      document.title = config.siteName;
+    }
+  }, [data]);
 
   if (data === undefined) {
     return <PageNotFound />;
